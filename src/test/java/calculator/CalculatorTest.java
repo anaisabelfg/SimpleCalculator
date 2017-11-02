ce1f0c7fd619d72db84823320807b5f76c6321de
Anfernandez <ana.fernandez@williamhill.com>
2017-11-02 18:24:34 +0100

    Initial Calculator Example

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,49 @@
+package calculator;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class CalculatorTest {
+
+    @Test
+    public void sum(){
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(5, 5);
+
+        //We want to do the more simple thing : add numbers
+        assertTrue(result == 10);
+    }
+
+
+    @Test
+    public void subtract() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.subtract(10, 5);
+
+        //We want to do the more simple thing : add numbers
+        assertTrue( result == 5);
+    }
+
+    @Test
+    public void multiply() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.multiply(10, 5);
+
+        //We want to do the more simple thing : add numbers
+        assertTrue(result == 50);
+    }
+
+    @Test
+    public void divide() {
+        Calculator calculator = new Calculator();
+
+        int result = calculator.divide(10, 5);
+
+        //We want to do the more simple thing : add numbers
+        assertTrue(result == 2);
+    }
+
+}
